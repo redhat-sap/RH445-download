3ed03a8c794f429b81f6ab135840d6f46cf03503
Markus Koch <mkoch@redhat.com>
2022-10-27 10:14:41 +0200

    update install script

--- download-sap-media.sh
+++ download-sap-media.sh
@@ -39,6 +39,7 @@ echo ""
 echo "Press Ctrl-C to stop downloading with S-User and provide the software on your own"
 echo "or enter your SAP credentials with download permissions"
 echo ""
+ansible-galaxy collection install ./collection/requirements.yml
 ansible-playbook -v -i utility, \
                  -u devops \
                  download-sap-media.yml 
