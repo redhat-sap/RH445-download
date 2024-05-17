d9c19b293a7f3b29e7f5aa8cafeb6190cddcb8c4
Markus Koch <mkoch@redhat.com#>
2024-05-17 09:15:12 +0200

    cleanup of solution

---
+++ solution/README.md
@@ -0,0 +1,7 @@
+# Solution
+
+This directory contains the playbooks that will be created during the RH445 course
+
+> [!NOTE] 
+> The scripts in this directory are already for the new version of the course
+with install_sap collection 1.4.0 and ansible 2.15+.
--- tools/update-lab.sh
+++ tools/update-lab.sh
@@ -11,3 +11,10 @@ echo -n "Press Ctrl-C to stop or Enter to continue"
 read ask
 
 ansible-playbook -v -K -i localhost,utility update-lab.yml
+
+cat << EOT
+===========================================================================
+Update done. Please run the following command or log off and on again:
+source ~/.bashrc
+===========================================================================
+EOT
